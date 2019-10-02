c7dd4a91287919e39db5b3a55d21c1f17e2d3135
dhon2407 <55686321+dhon2407@users.noreply.github.com>
2019-10-02 12:10:24 +0800

    Part 1
    - Change unity layout for easy portrait
    - Change Game display layout ratio (portrait for phone)
    - Add sprite for background (Create background)
    - Add sprite for bubble (Create bubble)
    - Move bubble
    - Move bubble on mouse click
    - Reset scene on keyboard press
    
    - limit re-move / change of direction
    - limit mouse click area

---
+++ Bubble Shooter/Assets/Scripts/BubbleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0;
+
+    private bool isMoving;
+    private Vector2 moveDirection;
+
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        if (isMoving)
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void StartMove(Vector2 direction)
+    {
+        if (isMoving) return;
+
+        isMoving = true;
+        moveDirection = direction;
+    }
+
+    public void StopMove()
+    {
+        isMoving = false;
+        moveDirection = Vector2.zero;
+    }
+}
---
+++ Bubble Shooter/Assets/Scripts/TapHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapHandler : MonoBehaviour
+{
+    [SerializeField]
+    private BubbleController mainBubble = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 worldTapPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 bubblePosition = mainBubble.transform.position;
+
+            if (worldTapPosition.y > bubblePosition.y + 1.5f)
+            {
+                Vector2 direction = (worldTapPosition - bubblePosition).normalized;
+                mainBubble.StartMove(direction);
+            }
+        }
+    }
+}
